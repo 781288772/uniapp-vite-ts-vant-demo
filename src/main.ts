d3c65f2c07e136cb5da471488a18ed872c4bf2a7
chen <781288772@qq.com>
2023-03-24 17:42:20 +0800

    feat:pinia持久化

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import Vant from 'vant';
 // 2. 引入组件样式
 import 'vant/lib/index.css';
 
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from "./App.vue";
 export function createApp() {
   const pinia = createPinia();
+  pinia.use(piniaPluginPersistedstate);
   const app = createSSRApp(App);
   app.use(Vant).use(pinia);
   return {
