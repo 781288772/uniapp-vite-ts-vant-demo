ffb53d0fca820f3131ba066aeffa9dd98f918035
chen <781288772@qq.com>
2023-03-31 17:43:00 +0800

    feat:处理爆红

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,5 @@
 // src/utils/http.js 中
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 import { UniAdapter } from "uniapp-axios-adapter";
 
 const request = axios.create({
@@ -14,7 +14,8 @@ request.interceptors.request.use((config:any) => {
   return config;
 });
 
-request.interceptors.response.use((response:any) => {
+//@ts-ignore
+request.interceptors.response.use((response) => {
   // 统一处理响应,返回Promise以便链式调用
   const { data } = response;
   if (response.status === 200) {
